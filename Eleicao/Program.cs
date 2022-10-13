09086eca755750d5ef76fdb95ef9485102d0d1a4
Android <Android@labfies.org.br>
2022-10-13 13:09:46 -0300

    eleicao

---
+++ Eleicao/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Eleicao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
